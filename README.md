41f92fba981389eebefb6e1841a19e6d344eeabd
Lisa Jean <MsJean@Lisas-Air.fios-router.home>
2022-06-02 10:18:47 -0400

    add cache and readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# budget-tracker
+# Budget-Tracker
+
+## User Story
+
+AS AN avid traveller
+I WANT to be able to track my withdrawals and deposits with or without a data/internet connection
+SO THAT my account balance is accurate when I am traveling
+
+## Acceptance Criteria
+
+GIVEN a Budget Tracker without an internet connection
+WHEN the user inputs an expense or deposit
+THEN they will receive a notification that they have added an expense or deposit
+WHEN the user reestablishes an internet connection
+THEN the deposits or expenses added while they were offline are added to their transaction history and their totals are updated
+
+## Review
+
+github repo: https://github.com/jeanl87/budget-tracker.git
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,34 +1,42 @@
-const APP_PREFIX = "Budget-Tracker"; 
+const APP_PREFIX = "Budget-Tracker";
 const VERSION = "version_01";
-const CACHE_NAME = APP_PREFIEX + VERSION
+const CACHE_NAME = APP_PREFIEX + VERSION;
+
 const FILES_TO_CACHE = [
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+];
 
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request:" + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log("responing with cache :" + e.request.url);
+        return request;
+      } else {
+        console.log("file is not cached, fetching;" + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
+});
 
-
-
-]; 
-
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request:' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request){
-            if (request) {
-                console.log('responing with cache :' +e.request.url)
-                return request
-            } else{
-                console.log('file is not cached, fetching;' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-} )
-
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache){
-            console.log('installing cache:' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
-})
-
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache:" + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
